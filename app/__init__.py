3caaf575b4cc6b6a0a62dd0d193f9ebdb3ffa6da
Roseoketch <oketchrose@gmail.com>
2018-04-12 11:38:48 +0300

    sending emails

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 def create_app(config_name):
     app = Flask(__name__)
     from .auth import auth as auth_blueprint
@@ -20,6 +22,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     login_manager.session_protection = 'strong'
     login_manager.login_view = 'auth.login'
+    mail.init_app(app)
 
 
     from .main import main as main_blueprint
@@ -30,5 +33,5 @@ def create_app(config_name):
 
    # configure UploadSet
     configure_uploads(app,photos)
-    
+
     return app
